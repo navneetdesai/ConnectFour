b200e00cfa120908e8b7dbb5d06b982600332b54
Navneet Desai <navneetdesai44@gmail.com>
2022-06-16 04:37:09 -0400

    Create Tokens enum Create drop_token method Replace np array with python list

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from enum import Enum
 
 
 class Constants:
@@ -6,10 +7,23 @@ class Constants:
     COLUMNS = 7
 
 
+class Tokens(Enum):
+    RED = '🔴'
+    BLACK = '⚫'
+    WHITE = '⚪'
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return self.value
+
+
 class ConnectFour:
     def __init__(self, rows=Constants.ROWS, columns=Constants.COLUMNS):
-        self.board = np.zeros((rows, columns))
+        self.board = [[Tokens.WHITE] * Constants.COLUMNS] * Constants.ROWS
         self.players = [None] * 2
+        self.tokens = [Tokens.RED, Tokens.BLACK]
         self.turn = 0
         self.top_filled_rows = [Constants.ROWS - 1] * Constants.COLUMNS
         self.game_over = False
@@ -22,9 +36,20 @@ class ConnectFour:
         is_valid_column = not self.board[0][column]
         return is_on_board and is_valid_column
 
+    def drop_token(self, column, piece):
+        row = self.top_filled_rows[column]
+        print(row)
+        self.board[row][column] = piece
+
     def start_game(self):
         while not self.game_over:
+            print(self.board)
             column = int(input(f'{self.players[self.turn]}\'s turn (1 - 7): '))
+            if column == -1:
+                break
+            if self.is_valid_choice(column):
+                print('here')
+                self.drop_token(column, self.tokens[self.turn])
             self.turn = (self.turn + 1) % 2
 
 

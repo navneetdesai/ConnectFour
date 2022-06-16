5dc18d14f8f9dd49385e0aaf62b7e4307531e874
Navneet Desai <navneetdesai44@gmail.com>
2022-06-16 04:16:46 -0400

    Create class Constants Create method add_players create choice validation method

--- src/main.py
+++ src/main.py
@@ -1,20 +1,37 @@
 import numpy as np
 
 
+class Constants:
+    ROWS = 6
+    COLUMNS = 7
+
+
 class ConnectFour:
-    def __init__(self, rows=6, columns=7):
+    def __init__(self, rows=Constants.ROWS, columns=Constants.COLUMNS):
         self.board = np.zeros((rows, columns))
+        self.players = [None] * 2
+        self.turn = 0
+        self.top_filled_rows = [Constants.ROWS - 1] * Constants.COLUMNS
         self.game_over = False
 
-    def start(self):
+    def add_players(self, player_one, player_two):
+        self.players[0], self.players[1] = player_one, player_two
+
+    def is_valid_choice(self, column):
+        is_on_board = 1 <= column <= 7
+        is_valid_column = not self.board[0][column]
+        return is_on_board and is_valid_column
+
+    def start_game(self):
         while not self.game_over:
-            pass
+            column = int(input(f'{self.players[self.turn]}\'s turn (1 - 7): '))
+            self.turn = (self.turn + 1) % 2
 
 
 def main():
     game = ConnectFour()
-    game.start()
-    print(game.board)
+    game.start_game()
+
 
 if __name__ == '__main__':
     main()

da26b92a9e5c8c67c9303a1f74f12b6fb24acdd3
Navneet Desai <navneetdesai44@gmail.com>
2022-07-17 18:22:02 -0400

    Add documentation

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
-import itertools
+"""
+Author: Navneet Desai
+ConnectFour Game
+"""
 import math
 import sys
 from enum import Enum
@@ -6,6 +9,9 @@ import pygame
 
 
 class Constants:
+    """
+    Defines constant dimensions and color codes
+    """
     ROWS = 6
     COLUMNS = 7
     SIZE = 150
@@ -14,9 +20,13 @@ class Constants:
     RED = 255, 0, 0
     WHITE = 255, 255, 255
     RADIUS = 45
+    WAIT = 10000
 
 
 class Tokens(Enum):
+    """
+    Defines game tokens
+    """
     RED = '🔴'
     BLACK = '⚫'
     WHITE = '⚪'
@@ -28,13 +38,62 @@ class Tokens(Enum):
         return self.value
 
 
+class Pygame:
+    """
+    Class that handles Pygame specific functionality
+    """
+    @staticmethod
+    def init_pygame():
+        """
+        Setup the pygame screen dimensions and font
+        :return:
+        """
+        pygame.init()
+        font = pygame.font.SysFont("Corbel", 75)
+        unit_size = 100
+        width = Constants.COLUMNS * unit_size
+        height = (1 + Constants.ROWS) * unit_size
+        size = width, height
+        screen = pygame.display.set_mode(size)
+        pygame.display.set_caption('Connect Four')
+        return screen, font
+
+    @staticmethod
+    def draw_circle(screen, color, row, column, radius):
+        """
+        Draws a circle of standard dimensions on a board with 'row' rows
+        and 'column' columns
+        """
+        pygame.draw.circle(screen, color, (column * 100 + 50, row * 100 + 100 + 50),radius)
+
+    @staticmethod
+    def draw_rectangle(screen, color, row, column):
+        """
+        Draws a rectangle of standard dimensions on a board with 'row' rows
+        and 'column' columns
+        """
+        pygame.draw.rect(screen, color, (column * 100, row * 100 + 100, 100, 100))
+
+
 class Board:
+    """
+    Represents the board used in the game
+    """
     def __init__(self, default_token, rows, columns):
+        """
+        Creates the board
+        :param default_token: default token the board should be filled with
+        :param rows: number of rows in the board
+        :param columns: number of columns in the board
+        """
         self.board = [[default_token for _ in range(columns)] for _ in range(rows)]
         self.top_filled_rows = [rows - 1 for _ in range(columns)]
 
     def __str__(self):
-        # To-do: store and replace instead of creating every time?
+        """
+        Representation of the board
+        :return:
+        """
         repr_ = []
         for row in self.board:
             repr_.extend(f'{str(token)} ' for token in row)
@@ -42,22 +101,29 @@ class Board:
         return ''.join(repr_)
 
     def get(self, row, column):
+        """
+        Returns the token at (row, column) position
+        """
         return self.board[row][column]
 
     def is_winning_move(self, row, column, token) -> bool:
+        """
+        Returns true if the current move is a winning move
+        :return:
+        """
         board = self.board
         for c in range(column - 3, column + 1):
-            count = sum(0 <= c + d < Constants.COLUMNS and board[row][c + d] == token for d in range(4))
+            count = sum(0 <= c + d < Constants.COLUMNS and board[row][c + d] == token \
+                        for d in range(4))
             if count == 4:
                 return True
 
-        # check vertical
         for r in range(row - 3, row + 1):
-            count = sum(0 <= r + d < Constants.ROWS and board[r + d][column] == token for d in range(4))
+            count = sum(0 <= r + d < Constants.ROWS and board[r + d][column] == token \
+                        for d in range(4))
             if count == 4:
                 return True
 
-        # check negative diagonal
         for r, c in zip(range(row - 3, row + 1), range(column - 3, column + 1)):
             count = 0
             for d in range(4):
@@ -67,7 +133,6 @@ class Board:
                 if count == 4:
                     return True
 
-        # check positive diagonal
         for r, c in zip(range(row + 3, row - 1, -1), range(column - 3, column + 1)):
             count = 0
             for d in range(4):
@@ -79,6 +144,14 @@ class Board:
         return False
 
     def drop_token(self, column, token) -> bool:
+        """
+        Drops a token in a particular column on
+        the board
+        Returns true if the move is a winning move
+        :param column: target column for the token
+        :param token: token to be dropped
+        :return: True if the move ends the game
+        """
         row = self.top_filled_rows[column]
         self.board[row][column] = token
         self.top_filled_rows[column] -= 1
@@ -86,7 +159,15 @@ class Board:
 
 
 class ConnectFour:
+    """
+    Represents the ConnectFour game
+    """
     def __init__(self, rows=Constants.ROWS, columns=Constants.COLUMNS):
+        """
+        Inits the game
+        :param rows: number of rows on the board
+        :param columns: number of columns on the board
+        """
         self.board = Board(Tokens.BLACK, rows, columns)
         self.players = [None] * 2
         self.tokens = [Tokens.RED, Tokens.WHITE]
@@ -94,39 +175,56 @@ class ConnectFour:
         self.game_over = False
 
     def add_players(self, player_one, player_two):
+        """
+        Adds players to the game
+        :param player_one:
+        :param player_two:
+        :return:
+        """
         self.players[0], self.players[1] = player_one, player_two
 
     def is_valid_choice(self, column):
+        """
+        Validates whether the choice of column
+        is valid
+        :param column: target column
+        :return: True if the move is valid
+        """
         is_on_board = 1 <= column <= 7
         is_valid_column = self.board.get(0, column) == Tokens.BLACK
         return is_on_board and is_valid_column
 
     def drop_token(self, column, token):
+        """
+        Drops a token in the column
+        """
         return self.board.drop_token(column, token)
 
     def draw_graphic_board(self, screen):
+        """
+        Draws the board on the screen
+        :param screen: pygame object
+        """
         for row in range(Constants.ROWS):
             for column in range(Constants.COLUMNS):
+                Pygame.draw_rectangle(screen, Constants.BLUE, row, column)
                 pygame.draw.rect(screen, Constants.BLUE, (column * 100, row * 100 + 100, 100, 100))
                 if self.board.get(row, column) == Tokens.RED:
-                    # print(1)
-                    pygame.draw.circle(screen, Constants.RED, (column * 100 + 50, row * 100 + 100 + 50),
-                                       Constants.RADIUS)
+                    Pygame.draw_circle(screen, Constants.RED, row, column, Constants.RADIUS)
                 elif self.board.get(row, column) == Tokens.WHITE:
-                    # print(2)
-                    pygame.draw.circle(screen, Constants.WHITE, (column * 100 + 50, row * 100 + 100 + 50),
-                                       Constants.RADIUS)
+                    Pygame.draw_circle(screen, Constants.WHITE, row, column, Constants.RADIUS)
                 else:
-                    # print(3)
-                    pygame.draw.circle(screen, Constants.BLACK, (column * 100 + 50, row * 100 + 100 + 50),
-                                       Constants.RADIUS)
+                    Pygame.draw_circle(screen, Constants.BLACK, row, column, Constants.RADIUS)
         pygame.display.update()
 
     def start(self):
+        """
+        Starts the game and loops over the turns
+        """
         player_one = input('Who\'s player one? - ')
-        player_two = input('Who\'s player two?- ')
+        player_two = input('Who\'s player two? - ')
         self.add_players(player_one, player_two)
-        screen, font = init_pygame()
+        screen, font = Pygame.init_pygame()
         self.draw_graphic_board(screen)
         pygame.display.update()
         while not self.game_over:
@@ -136,31 +234,21 @@ class ConnectFour:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     x, y = event.pos
                     column = math.floor(x / 100)
-                    if self.is_valid_choice(column) and self.drop_token(column, self.tokens[self.turn]):
+                    if self.is_valid_choice(column) and \
+                            self.drop_token(column, self.tokens[self.turn]):
                         print(f'{self.players[self.turn]} has won! Congratulations!!!')
-                        label = font.render(f"{self.players[self.turn]} wins!! Congratulations!", True, Constants.RED)
+                        label = font.render(f"{self.players[self.turn]} wins!! Congratulations!",
+                                            True, Constants.RED)
                         screen.blit(label, (40, 10))
                         self.game_over = True
                     self.turn = (self.turn + 1) % 2
                 self.draw_graphic_board(screen)
-        pygame.time.wait(5000)
+        pygame.time.wait(Constants.WAIT)
 
 
 def main():
     game = ConnectFour()
     game.start()
-
-
-def init_pygame():
-    pygame.init()
-    font = pygame.font.SysFont("Monospace", 75)
-    unit_size = 100
-    width = Constants.COLUMNS * unit_size
-    height = (1 + Constants.ROWS) * unit_size
-    size = width, height
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('Connect Four')
-    return screen, font
 
 
 if __name__ == '__main__':
